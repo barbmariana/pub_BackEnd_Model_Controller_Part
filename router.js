a088f8ae0fe054df8006af989e9030cf1199c823
Wesley ric <112405341+wesleyrick@users.noreply.github.com>
2023-03-03 11:44:26 -0300

    Finalizando Estoque

--- router.js
+++ router.js
@@ -1,41 +1,31 @@
 const express = require('express');
 const routes = express.Router();
-const produto = require('./src/controllers/ProdutosControllers')
-const funcionario = require('./src/controllers/FuncionariosController')
-const unidade = require("./src/controllers/UnidadesController");
+const produto = require ('./src/controllers/ProdutosControllers')
+const funcionario = require ('./src/controllers/FuncionariosController')
+const estoque = require ('./src/controllers/EstoqueController')
 
-//Routes index:
-// routes.get("/", (req, res) => {
-//     try {
-//         res.send("Bem vindo a API de nosso PUB")
-//     } catch {
-//         console.log("Problema ao carregar rota.")
-
-//     }
-// })
 
 //Routes Produtos:
 routes.post("/createProduto", produto.Create);
 routes.get("/listProduto", produto.List);
-routes.get("/searchProduto/:id", produto.FindOne);
-routes.put("/updateProduto/:id", produto.Update)
 
 //Routes Funcionários:
 
 routes
-.get("/funcionarios", funcionario.listarFuncionarios )
-.post("/funcionarios", funcionario.criarFuncionario)
-.get("/funcionarios/:id", funcionario.listarFuncionarioUnico)
-.put("/funcionarios/:id", funcionario.editarFuncionario)
-.delete("/funcionarios/:id", funcionario.deletarFuncionario)
+.get("/funcionarios", funcionario.ListarFuncionarios)
+.post("/funcionarios", funcionario.CriarFuncionario)
+.get("/funcionarios/:id", funcionario.ListarFuncionarioUnico)
+.put("/funcionarios/:id", funcionario.EditarFuncionario)
+.delete("/funcionarios/:id", funcionario.DeletarFuncionario)
+
+//Routes Estoque
+
+routes 
+.get("/estoque", estoque.ListarEstoque)
+.post("/estoque", estoque.CriarEstoque)
+.get("/estoque/:id", estoque.ListarEstoqueUnico)
+.put("/estoque/:id", estoque.EditarEstoque)
+.delete("/estoque/:id", estoque.DeletarEstoque)
 
 
-// Routes unidades
-routes.get('/unidades', unidade.listarUnidades);
-routes.get('/unidade/:id', unidade.listarUnidade);
-routes.post('/unidade', unidade.cadastrarUnidade);
-routes.put('/unidade/:id', unidade.atualizarUnidade);
-routes.delete('/unidade/:id', unidade.deletarUnidade);
-
-
-module.exports = routes
+module.exports = routes;
